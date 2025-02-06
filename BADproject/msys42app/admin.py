ad9db2ad83190c57436bf0118797916b67c7745d
ayenla <162469456+ayenla@users.noreply.github.com>
2025-02-06 23:55:01 +0800

    Delete BADproject directory

--- BADproject/msys42app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Child
-#UN: tanseis
-#PW: msys42
-admin.site.register(Child)
--- BADproject/msys42app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request,'msys42app/home.html')
-
-def create_child_profile(request):
-    return render(request,'msys42app/create_cp.html')
