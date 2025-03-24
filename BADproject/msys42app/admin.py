f48e8edac0552146d3b4e599cd5ca80db71ca4ff
ayenla <162469456+ayenla@users.noreply.github.com>
2025-03-24 19:31:38 +0800

    Update admin.py to include new models

--- BADproject/msys42app/admin.py
+++ BADproject/msys42app/admin.py
@@ -4,3 +4,8 @@ from .models import Child, ContactNumber
 #PW: msys42
 admin.site.register(Child)
 admin.site.register(ContactNumber)
+admin.site.register(PhysiciansExam)
+admin.site.register(Immunization)
+admin.site.register(AllergyCondition)
+admin.site.register(MedicalHistory)
+
