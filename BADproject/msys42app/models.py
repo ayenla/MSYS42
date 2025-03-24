1016f34e98efc481cca05cd733bcb9ea6a2d6958
ayenla <162469456+ayenla@users.noreply.github.com>
2025-03-24 19:35:16 +0800

    Update models.py to add physician's exam

--- BADproject/msys42app/models.py
+++ BADproject/msys42app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, RegexValidator # added on 2/26/2025, 03:38
+from django.core.validators import MinLengthValidator, RegexValidator 
 from django.utils import timezone
+import datetime
 
 class Child(models.Model):
     code = models.CharField(max_length=7, validators=[RegexValidator(regex=r'^[A-Za-z]{3}\d{4}$')], unique=True, null=False, blank=False)
@@ -71,3 +72,33 @@ class Immunization(models.Model):
     immunization_given = models.CharField(max_length=255)
 
 # END OF MEDICAL HISTORY
+
+# PHYSICIAN EXAM SECTION
+class PhysiciansExam(models.Model):
+    child = models.ForeignKey(Child, on_delete=models.CASCADE)
+    year_choices = [(year, year) for year in range(2000, datetime.datetime.now().year + 1)]
+    conditions = [("N", "N"), ("A", "A"), ("C", "C"), ("R", "R"), ("NE", "NE")]
+    year = models.IntegerField(choices=year_choices, default=datetime.datetime.now().year)
+    grade = models.CharField(max_length=2, choices=conditions, default= "NE")
+    height = models.CharField(max_length=2, choices=conditions, default= "NE")
+    weight = models.CharField(max_length=2, choices=conditions, default= "NE")
+    bp = models.CharField(max_length=2, choices=conditions, default= "NE")
+    vision_right = models.CharField(max_length=2, choices=conditions, default= "NE")
+    vision_left = models.CharField(max_length=2, choices=conditions, default= "NE")
+    hearing_right = models.CharField(max_length=2, choices=conditions, default= "NE")
+    hearing_left = models.CharField(max_length=2, choices=conditions, default= "NE")
+    eyes = models.CharField(max_length=2, choices=conditions, default= "NE")
+    ears = models.CharField(max_length=2, choices=conditions, default= "NE")
+    nose = models.CharField(max_length=2, choices=conditions, default= "NE")
+    throat = models.CharField(max_length=2, choices=conditions, default= "NE")
+    teeth = models.CharField(max_length=2, choices=conditions, default= "NE")
+    heart = models.CharField(max_length=2, choices=conditions, default= "NE")
+    lungs = models.CharField(max_length=2, choices=conditions, default= "NE")
+    abdomen = models.CharField(max_length=2, choices=conditions, default= "NE")
+    nervous_system = models.CharField(max_length=2, choices=conditions, default= "NE")
+    skin = models.CharField(max_length=2, choices=conditions, default= "NE")
+    nutrition = models.CharField(max_length=2, choices=conditions, default= "NE")
+    other = models.CharField(max_length=2, choices=conditions, default= "NE")
+    other_label = models.CharField(max_length=20, default="other")
+
+#END PHYSICIAN'S EXAM
