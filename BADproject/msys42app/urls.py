32af8d9efe157d218d3a834b61de959aa6900e15
ayenla <162469456+ayenla@users.noreply.github.com>
2025-04-24 14:57:48 +0800

    Added delete family member

--- BADproject/msys42app/urls.py
+++ BADproject/msys42app/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('family-medical-records/<int:pk>/view/<int:id>/', views.view_family_medical_record, name='view_family_medical_record'),
     path('family-medical-records/<int:pk>/edit/<int:id>/', views.edit_family_medical_record, name='edit_family_medical_record'),
     path('family-medical-records/<int:pk>/edit-family-information/<int:id>/', views.edit_family_info, name='edit_family_info'),
+    path('family-medical-records/<int:pk>/delete-family-member/<int:id>/', views.delete_family_member, name='delete_family_member'),
 
     # Medical History Paths
     path('medical-history/add/<int:child_id>/', views.add_medical_history, name='create_medical_history'),
